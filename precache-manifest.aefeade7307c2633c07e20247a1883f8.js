ef709e8d4ce7daf8d399763f55d0fd7f91f3493f
ABBY STARNES <abbystarnes@ABBYs-Air.lan>
2020-11-14 16:06:58 -0500

    Updates

---
+++ precache-manifest.aefeade7307c2633c07e20247a1883f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f70bcf4d5bf4efcaa2870181201595b",
+    "url": "http://abbystarnes.com/index.html"
+  },
+  {
+    "revision": "1a7b00102bb1784f4d14",
+    "url": "http://abbystarnes.com/static/css/main.b9aa8a40.chunk.css"
+  },
+  {
+    "revision": "c525606b6e3c4b1beaf8",
+    "url": "http://abbystarnes.com/static/js/2.83ee24c0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "http://abbystarnes.com/static/js/2.83ee24c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7b00102bb1784f4d14",
+    "url": "http://abbystarnes.com/static/js/main.f1e8ecee.chunk.js"
+  },
+  {
+    "revision": "ec8382a2ffc8273ae08c",
+    "url": "http://abbystarnes.com/static/js/runtime-main.33176147.js"
+  },
+  {
+    "revision": "93b2c9d18aa4dca2f0b3db14af18a865",
+    "url": "http://abbystarnes.com/static/media/hero.93b2c9d1.jpg"
+  }
+]);
