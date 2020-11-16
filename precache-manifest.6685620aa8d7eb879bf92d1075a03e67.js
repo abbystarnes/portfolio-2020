1f3808bf6abea31e0f8ff4068468698e7d9a7b70
ABBY STARNES <abbystarnes@ABBYs-Air.lan>
2020-11-15 22:01:40 -0500

    Updates

--- precache-manifest.6685620aa8d7eb879bf92d1075a03e67.js
+++ precache-manifest.6685620aa8d7eb879bf92d1075a03e67.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a45818e48f9147187a04ad0a57245fbd",
+    "revision": "44b95a6b4500fc5ce7389ea11ebdcf9b",
     "url": "https://abbystarnes.github.io/portfolio-2020/index.html"
   },
   {
-    "revision": "8d8aa73b115cd3431b73",
+    "revision": "cfc6ef4b77f568e69fdf",
     "url": "https://abbystarnes.github.io/portfolio-2020/static/css/main.b9aa8a40.chunk.css"
   },
   {
-    "revision": "a854f283429162b91233",
-    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/2.fd63b0e1.chunk.js"
+    "revision": "bb91dee1f30da1f16b1c",
+    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/2.9a9a0f69.chunk.js"
   },
   {
     "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
-    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/2.fd63b0e1.chunk.js.LICENSE.txt"
+    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/2.9a9a0f69.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8d8aa73b115cd3431b73",
-    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/main.9b6e7225.chunk.js"
+    "revision": "cfc6ef4b77f568e69fdf",
+    "url": "https://abbystarnes.github.io/portfolio-2020/static/js/main.c2bd4a32.chunk.js"
   },
   {
     "revision": "14fd20d229d3c0d1b055",
